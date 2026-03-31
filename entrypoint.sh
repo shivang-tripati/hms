8c2dcd925246d83d68ac21d4dbae39ff74b64ca2
shivang-tripati <shivangtripathi101@gmail.com>
2026-03-31 13:58:57 +0530

    added entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
-echo "Running Prisma migrations..."
-npx prisma migrate deploy
+echo "Waiting for database..."
+
+until ./node_modules/.bin/prisma migrate deploy; do
+  echo "Database not ready, retrying in 2s..."
+  sleep 2
+done
 
 echo "Starting Next.js server..."
 exec node server.js
